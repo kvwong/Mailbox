e2ee19c826aa953893f644ff9dda8f7eb19999a3
Kevin Wong <kdubz313@gmail.com>
2015-10-04 18:25:04 -0700

    Added tab views and composer UI
    Tab views with logic and animations
    Composer UI with dismiss
    Fixed bug where icon alpha was not changing near edges of swipe

--- Mailbox/FeedViewController.swift
+++ Mailbox/FeedViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class FeedViewController: UIViewController {
-
+    
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var feedView: UIImageView!
     @IBOutlet weak var helpView: UIImageView!
@@ -28,7 +28,10 @@ class FeedViewController: UIViewController {
     @IBOutlet weak var listIcon: UIImageView!
     @IBOutlet weak var messageBackground: UIView!
     @IBOutlet weak var appView: UIView!
-
+    @IBOutlet weak var archiveView: UIImageView!
+    @IBOutlet weak var laterView: UIImageView!
+    @IBOutlet weak var composer: UIImageView!
+    @IBOutlet weak var blackout: UIView!
     
     var totalSize : CGFloat = 0.0
     var messageCenter: CGPoint!
@@ -52,12 +55,12 @@ class FeedViewController: UIViewController {
         menuStatus = false
         rescheduleStatus = false
         listStatus = false
-
+        
         totalSize = feedView.image!.size.height + helpView.image!.size.height + searchView.image!.size.height
         
         scrollView.setContentOffset(scrollPoint, animated: true)
         scrollView.contentSize.height = totalSize
-
+        
         messageBackground.backgroundColor = UIColor.greenColor()
         archiveIcon.alpha = 0
         deleteIcon.alpha = 0
@@ -66,12 +69,94 @@ class FeedViewController: UIViewController {
         rescheduleView.alpha = 0
         listView.alpha = 0
         menuView.alpha = 0
+        blackout.alpha = 0
+        laterView.alpha = 0
         
         let edgeGesture = UIScreenEdgePanGestureRecognizer(target: self, action: "onEdgePan:")
         edgeGesture.edges = UIRectEdge.Left
         appView.addGestureRecognizer(edgeGesture)
     }
-
+    
+    @IBAction func compose(sender: AnyObject) {
+        blackout.alpha = 0.5
+        UIView.animateWithDuration(0.3, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+            self.composer.frame.origin.y = 65
+            }, completion: nil)
+    }
+    
+    @IBAction func tabbing(sender: AnyObject) {
+        switch tabView.selectedSegmentIndex
+        {
+        case 0:
+            tabView.tintColor = UIColorFromHex(0xFFCC00, alpha: 1.0)
+            laterView.alpha = 1
+            
+            if scrollView.frame.origin.x == 0 {
+                archiveView.frame.origin.x = 320
+                
+                UIView.animateWithDuration(0.3) { () -> Void in
+                    self.laterView.frame.origin.x = 0
+                    self.scrollView.frame.origin.x = 320
+                }
+                
+            } else if archiveView.frame.origin.x == 0 {
+                scrollView.frame.origin.x = 320
+                
+                UIView.animateWithDuration(0.3) { () -> Void in
+                    self.laterView.frame.origin.x = 0
+                    self.archiveView.frame.origin.x = 320
+                }
+            }
+        case 1:
+            scrollView.alpha = 1
+            tabView.tintColor = UIColorFromHex(0x50B9DB, alpha: 1.0)
+            if laterView.frame.origin.x == 0 {
+                archiveView.frame.origin.x = 320
+                
+                UIView.animateWithDuration(0.3, animations: {
+                    self.scrollView.frame.origin.x = 0
+                    self.laterView.frame.origin.x = -320
+                    }, completion: {
+                        (finished: Bool) -> Void in
+                        self.laterView.alpha = 0
+                })
+            } else if archiveView.frame.origin.x == 0 {
+                laterView.frame.origin.x = -320
+                
+                UIView.animateWithDuration(0.3) { () -> Void in
+                    self.scrollView.frame.origin.x = 0
+                    self.archiveView.frame.origin.x = 320
+                }
+            }
+            
+        case 2:
+            tabView.tintColor = UIColorFromHex(0x62DA62, alpha: 1.0)
+            if laterView.frame.origin.x == 0 {
+                scrollView.frame.origin.x = -320
+                scrollView.alpha = 0
+                UIView.animateWithDuration(0.3, animations: {
+                    self.archiveView.frame.origin.x = 0
+                    self.laterView.frame.origin.x = -320
+                    }, completion: {
+                        (finished: Bool) -> Void in
+                        self.laterView.alpha = 0
+                })
+            } else if scrollView.frame.origin.x == 0 {
+                laterView.frame.origin.x = -320
+                laterView.alpha = 0
+                
+                UIView.animateWithDuration(0.3, animations: {
+                    self.archiveView.frame.origin.x = 0
+                    self.scrollView.frame.origin.x = -320
+                    }, completion: {
+                        (finished: Bool) -> Void in
+                        self.scrollView.alpha = 0
+                })
+            }
+        default:
+            break;
+        }
+    }
     
     @IBAction func menu(sender: AnyObject) {
         if menuStatus == false {
@@ -113,7 +198,7 @@ class FeedViewController: UIViewController {
                 self.messageView.userInteractionEnabled = false
                 UIView.animateWithDuration(0.3, delay: 0.0, usingSpringWithDamping: 0.65, initialSpringVelocity: 0.3, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
                     self.appView.frame.origin.x = self.appView.frame.origin.x + self.menuOffset
-                }, completion: nil)
+                    }, completion: nil)
             } else {
                 UIView.animateWithDuration(0.3, delay: 0.0, usingSpringWithDamping: 0.65, initialSpringVelocity: 0.3, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
                     self.menuView.alpha = 0
@@ -122,12 +207,12 @@ class FeedViewController: UIViewController {
                     self.messageView.userInteractionEnabled = true
                     self.appView.frame.origin.x = self.appView
                         .frame.origin.x - self.menuOffset
-                }, completion: nil)
+                    }, completion: nil)
             }
             print("Screen edge swiped!")
         }
     }
-
+    
     
     //Edge swipe the app to open the menu
     @IBAction func edgeSwipe(sender: UIScreenEdgePanGestureRecognizer) {
@@ -231,62 +316,66 @@ class FeedViewController: UIViewController {
         
         if sender.state == UIGestureRecognizerState.Began {
             messageCenter = messageView.center
+            archiveIcon.alpha = 0
+            laterIcon.alpha = 0
+            deleteIcon.alpha = 0
+            listIcon.alpha = 0
             
             //GESTURE CHANGED
         } else if sender.state == UIGestureRecognizerState.Changed {
             messageView.center = CGPoint(x: messageCenter.x + translation.x, y: messageCenter.y)
+            let archiveAlpha = (1.667 * messageView.frame.origin.x) / 100
+            let laterAlpha = (1.667 * fabs(messageView.frame.origin.x)) / 100
             
             if messageView.frame.origin.x > 0 && messageView.frame.origin.x < 60 {
                 messageBackground.backgroundColor = UIColorFromHex(0xE3E3E3, alpha: 1.0)
-                self.archiveIcon.alpha = 1.667 * messageView.frame.origin.x
+                archiveIcon.alpha = archiveAlpha
                 archiveIcon.frame.origin.x = 20
-                //archiveIcon.alpha = 0.5
-                //print(archiveIcon.alpha)
-                //print(messageView.frame.origin.x)
+                
+                print(archiveAlpha)
+                print(messageView.frame.origin.x)
             }
             else if messageView.frame.origin.x < 0 && messageView.frame.origin.x > -60 {
                 messageBackground.backgroundColor = UIColorFromHex(0xE3E3E3, alpha: 1.0)
-                laterIcon.alpha = 1
+                laterIcon.alpha = laterAlpha
                 laterIcon.frame.origin.x = 280
-                //print("nothing")
+                
+                print(laterAlpha)
+                print(messageView.frame.origin.x)
             }
-            
-            //Archive that shit
+                
+                //Archive that shit
             else if messageView.frame.origin.x > 60 && messageView.frame.origin.x < 260 {
                 archiveIcon.frame.origin.x = messageView.frame.origin.x - 40
                 archiveIcon.alpha = 1
                 deleteIcon.alpha = 0
                 messageBackground.backgroundColor = UIColorFromHex(0x62DA62, alpha: 1.0)
-                //print("archiving")
             }
-            
-            //Delete that shit
+                
+                //Delete that shit
             else if messageView.frame.origin.x > 260 {
                 deleteIcon.frame.origin.x = messageView.frame.origin.x - 40
                 deleteIcon.alpha = 1
                 archiveIcon.alpha = 0
                 messageBackground.backgroundColor = UIColorFromHex(0xEF540D, alpha: 1.0)
-                //print("deleting")
             }
-            
-            //Reschedule that shit
+                
+                //Reschedule that shit
             else if messageView.frame.origin.x < -60 && messageView.frame.origin.x > -260 {
                 laterIcon.frame.origin.x = messageView.frame.origin.x + 340
                 self.laterIcon.alpha = abs(1.667 * messageView.frame.origin.x)
                 listIcon.alpha = 0
                 messageBackground.backgroundColor = UIColorFromHex(0xFFCC00, alpha: 1.0)
-                //print("rescheduling")
             }
-            
-            //Put that shit in a list
+                
+                //Put that shit in a list
             else if messageView.frame.origin.x < -260 {
                 listIcon.frame.origin.x = messageView.frame.origin.x + 340
                 listIcon.alpha = 1
                 laterIcon.alpha = 0
                 messageBackground.backgroundColor = UIColorFromHex(0xD9A676, alpha: 1.0)
-                //print("listing")
             }
-
+            
             //GESTURE ENDED
         } else if sender.state == UIGestureRecognizerState.Ended {
             if messageView.frame.origin.x < 60 && messageView.frame.origin.x > 0 {
@@ -294,14 +383,14 @@ class FeedViewController: UIViewController {
                     self.messageView.frame.origin.x = 0
                     }, completion: nil)
             }
-        
+                
             else if messageView.frame.origin.x > -60 && messageView.frame.origin.x < 0 {
                 UIView.animateWithDuration(0.3, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5.0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
                     self.messageView.frame.origin.x = 0
                     }, completion: nil)
             }
-
-            //Complete ARCHIVE Animation
+                
+                //Complete ARCHIVE Animation
             else if messageView.frame.origin.x > 60 && messageView.frame.origin.x < 260 {
                 messageBackground.backgroundColor = UIColorFromHex(0x62DA62, alpha: 1.0)
                 archiveIcon.alpha = 0
@@ -319,10 +408,10 @@ class FeedViewController: UIViewController {
                             self.messageBackground.frame.size.height = 0
                             }, completion: nil)
                 })
-
+                
             }
-            
-            //Complete DELETE Animation
+                
+                //Complete DELETE Animation
             else if messageView.frame.origin.x > 260 {
                 messageBackground.backgroundColor = UIColorFromHex(0xEF540D, alpha: 1.0)
                 archiveIcon.alpha = 0
@@ -340,11 +429,11 @@ class FeedViewController: UIViewController {
                             self.messageBackground.frame.size.height = 0
                             }, completion: nil)
                 })
- 
-
+                
+                
             }
-            
-            //Complete RESCHEDULE Animation
+                
+                //Complete RESCHEDULE Animation
             else if messageView.frame.origin.x < -60 && messageView.frame.origin.x > -260 {
                 messageBackground.backgroundColor = UIColorFromHex(0xFFCC00, alpha: 1.0)
                 archiveIcon.alpha = 0
@@ -364,14 +453,14 @@ class FeedViewController: UIViewController {
                         //    self.messageBackground.frame.size.height = 0
                         //    }, completion: nil)
                         //} else {
-                            
+                        
                         //}
                 })
-
-
+                
+                
             }
-            
-            //Complete LIST Animation
+                
+                //Complete LIST Animation
             else if messageView.frame.origin.x < -260 {
                 messageBackground.backgroundColor = UIColorFromHex(0xD9A676, alpha: 1.0)
                 archiveIcon.alpha = 0
@@ -386,13 +475,19 @@ class FeedViewController: UIViewController {
                         (finished: Bool) -> Void in
                 })
                 
-
+                
             }
-    
+            
         }
     }
     
-
+    @IBAction func dismissComposer(sender: AnyObject) {
+        UIView.animateWithDuration(0.15, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+            self.composer.frame.origin.y = 568
+            self.blackout.alpha = 0
+            }, completion: nil)
+    }
+    
     @IBAction func dismissRescheduleView(sender: AnyObject) {
         if rescheduleView.alpha == 1 {
             rescheduleView.alpha = 0
@@ -421,5 +516,5 @@ class FeedViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-
+    
 }
